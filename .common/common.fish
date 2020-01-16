e8b382c9d06797c1e73c99ca5faacef901516207
aceforeverd <teapot@aceforeverd.com>
2020-01-17 00:50:18 +0800

    fish path functions

--- .common/common.fish
+++ .common/common.fish
@@ -6,3 +6,27 @@ if not functions -q fisher
 end
 
 set -x GPG_TTY (tty)
+
+# see https://superuser.com/questions/776008/how-to-remove-a-path-from-path-variable-in-fish
+if not functions -q addpaths
+    function addpaths
+        contains -- $argv $fish_user_paths
+        or set -U fish_user_paths $fish_user_paths $argv
+        echo "Updated PATH: $PATH"
+    end
+
+    funcsave addpaths
+end
+
+if not functions -q removepath
+    function removepath
+        if set -l index (contains -i $argv[1] $PATH)
+            set --erase --universal fish_user_paths[$index]
+            echo "Updated PATH: $PATH"
+        else
+            echo "$argv[1] not found in PATH: $PATH"
+        end
+    end
+
+    funcsave removepath
+end
