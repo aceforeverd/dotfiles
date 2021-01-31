0b791054ab09b4b5a4c5917f04a061fb449fb95b
aceforeverd <teapot@aceforeverd.com>
2021-01-31 17:14:29 +0800

    what

--- .common/common.fish
+++ .common/common.fish
@@ -4,6 +4,51 @@ if not functions -q fisher
     fish -c fisher
 end
 
+function fish_path_add
+    set -l cnt (count $argv 2> /dev/null)
+    if test $cnt -ne 2
+        set_color red; echo -e usage: $_ \$fish_variable \$path; set_color normal
+        return
+    end
+
+    set -l fish_variable $argv[1]
+    set -l pth $argv[2]
+    if ! test -d $pth
+        set_color red; echo -e \'$pth\' not a existing path; set_color normal
+    else
+        set -l pth (realpath -s $pth)
+        fish -c """
+        if contains -- $pth \$$fish_variable
+            set_color yellow; echo -e $pth already added; set_color normal
+        else
+            set -U $fish_variable \$$fish_variable $pth
+            set_color green; echo -e added $pth to $fish_variable; set_color normal
+        end
+        """
+    end
+end
+
+function fish_path_rm
+    set -l cnt (count $argv 2> /dev/null)
+    if test $cnt -ne 2
+        set_color red; echo -e usage: $_ \$fish_variable \$path; set_color normal
+        return
+    end
+
+    set -l fish_variable $argv[1]
+    set -l pth (realpath -s $argv[2] 2> /dev/null); or set -l pth $argv[2]
+
+    # must use "$fish_variable"[$index] in set -e, others seems not work
+    fish -c "
+    if set -l index (contains -i $pth \$$fish_variable)
+        set -e -U "$fish_variable"[$index]
+        set_color green; echo -e removed $pth from $fish_variable; set_color normal
+    else
+        set_color red; echo -e \'$pth\' not found in $fish_variable: \$$fish_variable; set_color normal
+    end
+    "
+end
+
 # see https://superuser.com/questions/776008/how-to-remove-a-path-from-path-variable-in-fish
 if not functions -q addpaths
     function addpaths
@@ -50,6 +95,26 @@ if not functions -q removepaths
     end
 
     funcsave removepaths
+end
+
+if not functions -q fish_complete_path_add
+    function fish_complete_path_add
+        for ph in $argv
+            fish_path_add fish_complete_path $ph
+        end
+    end
+
+    funcsave fish_complete_path_add
+end
+
+if not functions -q fish_complete_path_rm
+    function fish_complete_path_rm
+        for ph in $argv
+            fish_path_rm fish_complete_path $ph
+        end
+    end
+
+    funcsave fish_complete_path_rm
 end
 
 set -x GPG_TTY (tty)
