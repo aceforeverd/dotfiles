ec19fe3525f1e6373f8cde6e1d8f4a68a55438be
aceforeverd <teapot@aceforeverd.com>
2021-02-15 20:56:01 +0800

    Test with Fishtape (#3)

--- .common/common.fish
+++ .common/common.fish
@@ -1,57 +1,79 @@
 if not functions -q fisher && status is-interactive
     curl -sL https://git.io/fisher | source; and fisher install jorgebucaran/fisher
-    set_color yellow; echo automatically installed fisher; set_color normal
+    isatty; and set_color yellow
+    echo automatically installed fisher
+    isatty; and set_color normal
 end
 
+# add the given path into specified fish variables
+# return 0 on success, 1 on path not exist/invalid path, 2 on path already added, 3 others
 function fish_path_add
     set -l cnt (count $argv 2> /dev/null)
     if test $cnt -ne 2
-        set_color red; echo -e usage: $_ \$fish_variable \$path; set_color normal
-        return
+        isatty; and set_color red
+        echo -e usage: $_ \$fish_variable \$path
+        isatty; and set_color normal
+        return 3
     end
 
     set -l fish_variable $argv[1]
     set -l pth $argv[2]
     if ! test -d $pth
-        set_color red; echo -e \'$pth\' not a existing path; set_color normal
+        isatty; and set_color red
+        echo -e \'$pth\' not a existing path
+        isatty; and set_color normal
+        return 1
     else
         set -l pth (realpath -s $pth)
-        fish -c """
-        if contains -- $pth \$$fish_variable
-            set_color yellow; echo -e $pth already added; set_color normal
+        if fish -c "contains -- $pth \$$fish_variable"
+            isatty; and set_color yellow
+            echo -e $pth already added in $fish_variable
+            isatty; and set_color normal
+            return 2
         else
-            set -U $fish_variable \$$fish_variable $pth
-            set_color green; echo -e added $pth to $fish_variable; set_color normal
+            fish -c "set -U $fish_variable \$$fish_variable $pth"
+            isatty; and set_color green
+            echo -e added $pth to $fish_variable
+            isatty; and set_color normal
+            return 0
         end
-        """
     end
 end
 
+# remove path from fish variables
+# return 0 on success, 1 if path not exist/invalid, 3 others
 function fish_path_rm
     set -l cnt (count $argv 2> /dev/null)
     if test $cnt -ne 2
-        set_color red; echo -e usage: $_ \$fish_variable \$path; set_color normal
-        return
+        isatty; and set_color red
+        echo -e usage: $_ \$fish_variable \$path
+        isatty; and set_color normal
+        return 3
     end
 
     set -l fish_variable $argv[1]
     set -l pth (realpath -s $argv[2] 2> /dev/null); or set -l pth $argv[2]
 
-
     set -l index (fish -c "contains -i $pth \$$fish_variable; or echo 0")
     if test $index -gt 0
         set -l path_removing "$fish_variable"[$index]
         fish -c "set -e -U $path_removing"
-        set_color green; echo -e removed $pth from $fish_variable; set_color normal
+        isatty; and set_color green
+        echo -e removed $pth from $fish_variable
+        isatty; and set_color normal
+        return 0
     else
-        set_color red; echo -e \'$pth\' not found in $fish_variable; set_color normal
+        isatty; and set_color red
+        echo -e \'$pth\' not found in $fish_variable
+        isatty; and set_color normal
+        return 1
     end
 end
 
 if not functions -q fish_user_paths_add
     function fish_user_paths_add
         for ph in $argv
-            fish_path_add fish_user_paths $ph
+            fish_path_add fish_user_paths $ph; or return $status
         end
     end
 
@@ -61,7 +83,7 @@ end
 if not functions -q fish_user_paths_rm
     function fish_user_paths_rm
         for ph in $argv
-            fish_path_rm fish_user_paths $ph
+            fish_path_rm fish_user_paths $ph; or return $status
         end
     end
 
@@ -71,7 +93,7 @@ end
 if not functions -q fish_complete_path_add
     function fish_complete_path_add
         for ph in $argv
-            fish_path_add fish_complete_path $ph
+            fish_path_add fish_complete_path $ph; or return $status
         end
     end
 
@@ -81,7 +103,7 @@ end
 if not functions -q fish_complete_path_rm
     function fish_complete_path_rm
         for ph in $argv
-            fish_path_rm fish_complete_path $ph
+            fish_path_rm fish_complete_path $ph; or return $status
         end
     end
 
---
+++ test/common_test.fish
@@ -0,0 +1,36 @@
+set -l user_paths $fish_user_paths
+
+set -l TEMP_DIR /tmp/(openssl rand -base64 8)
+set -l NON_EXIST_PATH /tmp/(openssl rand -base64 8)
+mkdir -p $TEMP_DIR
+
+echo creating directory $TEMP_DIR
+
+@test "fish_user_paths: not contains me" (contains -- $TEMP_DIR $fish_user_paths) $status -ne 0
+
+@test "fish_user_paths_add: it tell me added" (fish_user_paths_add $TEMP_DIR > test.log) $status -eq 0
+echo -n -e "\t"
+cat test.log
+
+@test "fish_user_paths_add: really added" (contains -- $TEMP_DIR $fish_user_paths) $status -eq 0
+
+@test "fish_user_paths_add: avoid add multiple times" (fish_user_paths_add $TEMP_DIR > test.log) $status -eq 2
+echo -n -e "\t"
+cat test.log
+
+@test "fish_user_paths_add: still have me" (contains -- $TEMP_DIR $fish_user_paths) $status -eq 0
+
+@test "path not exist" ! -d $NON_EXIST_PATH
+@test "fish_user_paths_add: add non-exist path" (fish_user_paths_add $NON_EXIST_PATH > test.log) $status -eq 1
+echo -n -e "\t"
+cat test.log
+@test "fish_user_paths_add: really not added" (contains -- $NON_EXIST_PATH $fish_user_paths) $status -ne 0
+
+@test "fish_user_paths_rm: removing not exist path" (fish_user_paths_rm $NON_EXIST_PATH > /dev/null) $status -eq 1
+@test "fish_user_paths_rm: removed" (fish_user_paths_rm $TEMP_DIR > test.log) $status -eq 0
+echo -n -e "\t"
+cat test.log
+
+rm -r $TEMP_DIR
+rm test.log
+echo removed $TEMP_DIR
